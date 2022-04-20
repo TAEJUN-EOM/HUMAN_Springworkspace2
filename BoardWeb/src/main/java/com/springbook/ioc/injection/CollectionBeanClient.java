1266462feb88733e305ce83628f5f1fdd655d770
Psychopomp <91600307+TAEJUN-EOM@users.noreply.github.com>
2022-04-20 22:42:02 +0900

    Springworkspace2_BoardWeb

---
+++ BoardWeb/src/main/java/com/springbook/ioc/injection/CollectionBeanClient.java
@@ -0,0 +1,25 @@
+package com.springbook.ioc.injection;
+
+import java.util.Properties;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class CollectionBeanClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AbstractApplicationContext factory = 
+				new GenericXmlApplicationContext("applicationContext.xml");
+		
+		CollectionBean  bean = (CollectionBean)factory.getBean("collectionBean");
+		
+		Properties addressList = bean.getAddressList();
+		
+		for(String key : addressList.stringPropertyNames()) {
+			System.out.println(String.format("키 :%s, 값 : %s",key ,addressList.get(key)));
+		}
+		
+	}
+
+}
---
+++ BoardWeb/src/main/java/polymorphism/LgTV.java
@@ -0,0 +1,47 @@
+package polymorphism;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component("tv")
+public class LgTV implements TV{
+	
+	/*
+	 * @Autowired
+	 * 
+	 * @Qualifier("apple")
+	 */
+	//@Resource(name="apple")
+	
+	@Autowired
+	private Speaker speaker;
+	
+	@Override
+	public void powerOn() {
+		
+		System.out.println("LgTV---전원 켠다.");
+	}
+
+	@Override
+	public void powerOff() {
+		
+		System.out.println("LgTV---전원 끈다.");
+	}
+
+	@Override
+	public void volumeUp() {
+		
+		//System.out.println("LgTV---소리 올린다.");
+		speaker.volumeUp();
+	}
+
+	@Override
+	public void volumeDown() {
+		
+		//System.out.println("LgTV---소리 내린다.");
+		speaker.volumeDown();
+	}
+}
